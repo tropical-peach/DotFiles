ac861409be9ec04e39e3441c8700b6e289815472
⛆PEACH⛆ <tropical-peach@users.noreply.github.com>
2017-06-13 11:13:34 -0400

    Create Initalize_Linux.sh

--- Initalize_Linux.sh
+++ Initalize_Linux.sh
@@ -8,11 +8,11 @@
 # ████████ ░░██████  ░░██ ░░██████░██       ░████████░██ ███  ░██░░██████ ██ ░░██
 #░░░░░░░░   ░░░░░░    ░░   ░░░░░░ ░░        ░░░░░░░░ ░░ ░░░   ░░  ░░░░░░ ░░   ░░ 
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+mkdir ~/gits
 sudo apt-get update
-sudo apt-get install google-chrome-stable rxvt-unicode-256color compton toilet figlet git vim clang gcc g++ cmake curl zsh stow
+sudo apt-get install rxvt-unicode-256color compton toilet figlet git clang gcc g++ cmake curl zsh stow
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/vim/vim.git ~/gits/vim
 mv ~/.bashrc ~/.bashrc.BKP
 stow ./home_resources -t ~/
 stow ./fonts -t /user/share/figlet/fonts/
@@ -20,4 +20,5 @@ cd /usr/lib/urxvt && sudo rm -r perl/ && sudo git clone https://github.com/tropi
 
 
 printf "Ensure fonts and other path dependant files are installed"
+printf "Don't forget to compile VIM"
 echo "----------"
