153f38a24157d33ce89c2bf96cc5d5cd55a3bd51
tropical-peach <steven.t.seppala@gmail.com>
2017-06-13 11:10:23 -0400

    Fixed man color issue

--- home_resources/bin/shell/5_mancolors.sh
+++ home_resources/bin/shell/5_mancolors.sh
@@ -11,7 +11,7 @@
 #       and chmod +x it(it also gets run as a script)
 #       If you name it something else or put it somewhere else,
 #       change this:
-export MANCOLORPATH=$HOME/.mancolor.sh
+export MANCOLORPATH=$HOME/bin/shell/mancolors.sh
 #If you want it to use just the man section as the seed change this
 # to 1. Man pages of the same section will have the same schemes.
 export USESECTION=0
